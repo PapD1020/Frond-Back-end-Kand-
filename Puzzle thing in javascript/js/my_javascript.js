a1e36f4a611ec70e488da97540d45407d8f2e29e
PapD1020 <papd@kkszki.hu>
2021-10-05 15:38:28 +0200

    random generátor, táblázat random számokkal való feltöltése

--- Puzzle thing in javascript/js/my_javascript.js
+++ Puzzle thing in javascript/js/my_javascript.js
@@ -1,3 +1,17 @@
-function myFunction(){
-    alert("asd");
+//Random szám generálása, min és max is beletartozik
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+//Random számokkal a 4x4-es tábla egyes mezőinek feltöltése
+function generateNumbers(){
+    for(let i = 0; i < 1; i++){
+        console.log("i: " + i);
+        for(let j = 0; j < 16; j++){
+            console.log("j: " + j);
+            tmp = "" + i + j;
+            console.log("tmp: " + tmp);
+            document.getElementById(tmp).innerHTML = getRandomInteger(1, 16);
+        }
+    }
 }
