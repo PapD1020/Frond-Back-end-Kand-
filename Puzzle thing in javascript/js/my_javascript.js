806950cd751ab2b614f0341a55edb31c63842c98
PapD1020 <papd@kkszki.hu>
2021-10-06 19:46:43 +0200

    Alternatív4 egyedi megoldás (dowhile-ban a while érdekes)

--- Puzzle thing in javascript/js/my_javascript.js
+++ Puzzle thing in javascript/js/my_javascript.js
@@ -4,6 +4,7 @@ function getRandomInteger(min, max) {
   }
 
 //Random számokkal a 4x4-es tábla egyes mezőinek feltöltése
+/*
 function generateNumbers(){
     for(let i = 0; i < 1; i++){
         console.log("i: " + i);
@@ -15,12 +16,63 @@ function generateNumbers(){
         }
     }
 }
-
-//Alternatív
+*/
 function generateNumbers2(){
     for(let i = 1; i <= 16; i++){
+        tmp = "" + i;
+        document.getElementById(tmp).innerHTML = getRandomInteger(1, 16);
+    }
+}
+//Alternatív3
+function generateNumbers3(){
+    document.getElementById("1").innerHTML = getRandomInteger(1, 16);
+    //console.log("első szám: " + document.getElementById("1").innerHTML);
+    for(let i = 2; i <= 16; i++){
         console.log("i: " + i);
         tmp = "" + i;
-        document.getElementById(tmp).innerHTML = getRandomInteger(1, 16); 
+        do {
+            num = getRandomInteger(1, 16);
+            document.getElementById(tmp).innerHTML = num;
+            //console.log("random szám: " + num);
+        } while (document.getElementById(tmp).innerHTML == document.getElementById(tmp - 1).innerHTML);
+
+        //console.log("ami beírtásra került: " + document.getElementById(tmp).innerHTML);
     }
+}
+
+//Alternatív4
+const numbersSet = new Set();
+const numbersSetTmp = new Set();
+function generateNumbers4(){
+    firstNumber = getRandomInteger(1, 16);
+    document.getElementById("1").innerHTML = firstNumber;
+    numbersSet.add(firstNumber);
+
+    console.log("első szám: " + firstNumber);
+
+    for(let i = 2; i <= 16; i++){
+        tmp = "" + i;
+        x = getRandomInteger(1, 16);
+        numbersSetTmp.add(x);
+        console.log("x: " + x);
+        if(numbersSet.has(x)){
+            do {
+                y = getRandomInteger(1, 16);
+                console.log("y: " + y);
+            } while (numbersSet.has(y));
+            document.getElementById(tmp).innerHTML = y;
+            numbersSet.add(y);
+
+            console.log("beírt szám: " + document.getElementById(tmp).innerHTML);
+        }
+        else{
+            document.getElementById(tmp).innerHTML = x;
+            numbersSet.add(x);
+
+            console.log("beírt szám: " + document.getElementById(tmp).innerHTML);
+        }
+    }
+
+    numbersSet.clear();
+    numbersSetTmp.clear();
 }
