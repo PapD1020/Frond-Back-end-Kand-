b81c455038a5a85569f0f6a13a2e7bd5ebd6e505
Pap <papd@kkszki.hu>
2021-11-03 10:11:51 +0100

    new item button added

--- 11.03/js/main.js
+++ 11.03/js/main.js
@@ -1,5 +1,8 @@
 var listItems = document.getElementById("list").getElementsByTagName("li");
 var ourHeader = document.getElementById("header");
+var ourButton = document.getElementById("buttonAddNewItem");
+var ourList = document.getElementById("list");
+var newItemCounter = listItems.length + 1;
 
 for(var i = 0; i < listItems.length; i++){
     listItems[i].addEventListener("click", activateItem);
@@ -7,4 +10,12 @@ for(var i = 0; i < listItems.length; i++){
 
 function activateItem(){
     ourHeader.innerText = this.innerText;
+}
+
+ourButton.addEventListener("click", createNewItem);
+
+function createNewItem(){
+
+    ourList.innerHTML += "<li>" + newItemCounter + "</li>";
+    newItemCounter++;
 }
