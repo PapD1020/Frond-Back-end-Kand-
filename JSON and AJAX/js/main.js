f17bba9d267f226a8e81f047413afd59f59cf38d
Pap <papd@kkszki.hu>
2021-11-03 11:48:17 +0100

    req

---
+++ JSON and AJAX/js/main.js
@@ -0,0 +1,38 @@
+/*
+var cat = {
+    "name": "Menta",
+    "species": "Felis catus",
+    "gender": "They are not 18 yes."
+}
+
+//tömb
+var colors = ["black", "grey", "green"];
+*/
+
+//JSON JavaScript Object Notation
+var pets = [
+    {
+        "name": "Menta",
+        "species": "Felis catus",
+        "gender": "They are not 18 yes."
+    },
+    {
+        "name": "Maci",
+        "species": "Canis lupus familiaris",
+        "gender": "They are not 18 yes."
+    },
+    {
+        "name": "Hamu",
+        "species": "Felis catus",
+        "gender": "They are not 18 yes."
+    }
+];
+
+//Hivatkozás JSON-ra
+pets[1].species;
+
+var req = new XMLHttpRequest();
+req.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+req.onload = function{
+    console.log(req);
+}
