075c8c2afc363422e901c44fb6c1f75700d4639d
Pap <papd@kkszki.hu>
2021-11-08 09:35:13 +0100

    added Core.Sqlite nuget

--- RandiApp/API/Data/DataContext.cs
+++ RandiApp/API/Data/DataContext.cs
@@ -1,12 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using API.Entities;
 
 namespace API.Data
 {
-    public class DataContext : DbContext{
-        
+    public class DataContext : DbContext
+    {
+        public DataContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<AppUser> Users{get;set;}
     }
 }
