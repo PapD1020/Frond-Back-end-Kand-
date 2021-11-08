1804c37a6369626d5a9f7f63d32f1303ca5a3aa1
Pap <papd@kkszki.hu>
2021-11-08 12:50:51 +0100

    HttpGet-s made async

--- RandiApp/API/Controllers/UsersController.cs
+++ RandiApp/API/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using API.Data;
 using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -19,14 +20,14 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers(){
-            return _context.Users.ToList(); 
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            return await _context.Users.ToListAsync(); 
         }
 
         //api/users/2
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUsers(int id){
-            return _context.Users.Find(id);
+        public async Task<ActionResult<AppUser>> GetUsers(int id){
+            return await _context.Users.FindAsync(id);
         }
     }
 }
