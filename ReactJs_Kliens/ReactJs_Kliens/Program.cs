bbafd5b1f548ded78ebdaa7acc0c5050bb93d820
Pap <papd@kkszki.hu>
2021-11-29 08:14:46 +0100

    ReactJs_Kliens new project

---
+++ ReactJs_Kliens/ReactJs_Kliens/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ReactJs_Kliens.ServiceReference1;
+
+namespace ReactJs_Kliens
+{
+    class Program
+    {
+        public static ServiceReference1.Service1Client kliens;
+        static void Main(string[] args)
+        {
+            kliens = new ServiceReference1.Service1Client();
+            Kutyus egykutyu = kliens.EgyKutyaGet();
+            Console.WriteLine(egykutyu);
+
+            Console.ReadKey();
+        }
+    }
+}
