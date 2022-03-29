2e894a270812d56533b07734e19a6e111793d75c
PapD1020 <71826326+PapD1020@users.noreply.github.com>
2022-03-29 09:37:33 +0200

    Add files via upload

---
+++ StudentProject/StudentProject/Services/StudentService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentProject.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentProject.Services
+{
+    public class StudentService
+    {
+        private readonly school2Context context;
+
+        public StudentService(school2Context context)
+        {
+            this.context = context;
+            //context.ChangeTracker.LazyLoadingEnabled = false;
+        }
+
+        public IEnumerable<Student> GetAllStudent()
+        {
+            return context.Students.Include(student => student.Subjects).ToList();
+        }
+
+        public Student GetStudentByEmail(int id)
+        {
+            //return context.Students.First(student => student.Id == id);
+            return context.Students.Include(s => s.Subjects).First(s => s.Id == id);
+        }
+    }
+}
