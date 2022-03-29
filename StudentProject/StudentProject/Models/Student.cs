81d32ce24dc4dd383b397579c0f6a61a83899b63
PapD1020 <71826326+PapD1020@users.noreply.github.com>
2022-03-29 09:26:17 +0200

    Add files via upload

---
+++ StudentProject/StudentProject/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+#nullable disable
+
+namespace StudentProject.Models
+{
+    public partial class Student
+    {
+        public Student()
+        {
+            Subjects = new HashSet<Subject>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public virtual ICollection<Subject> Subjects { get; set; }
+    }
+}
---
+++ StudentProject/StudentProject/Models/Subject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+#nullable disable
+
+namespace StudentProject.Models
+{
+    public partial class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //[JsonIgnore] ezt is lehet ignorálni
+        public int StudentId { get; set; }
+
+        [JsonIgnore]
+        public virtual Student Student { get; set; }
+    }
+}
