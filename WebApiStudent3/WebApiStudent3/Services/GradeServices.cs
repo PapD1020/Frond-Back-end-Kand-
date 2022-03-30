83465005b80599dd2485434f560940b872e56331
PapD1020 <71826326+PapD1020@users.noreply.github.com>
2022-03-30 09:05:04 +0200

    Add files via upload

---
+++ WebApiStudent3/WebApiStudent3/Services/GradeServices.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using WebApiStudent3.Models;
+
+namespace WebApiStudent3.Services
+{
+    public class GradeServices
+    {
+        private readonly school3Context context;
+
+        public GradeServices(school3Context context)
+        {
+            this.context = context;
+        }
+
+        public Grade GetGradesByStudentId(int id)
+        {
+            return context.Grades.First(Grade => Grade.StudentId == id);
+            //return context.Grades.Include(Grade => Grade.StudentId == id).First(Student => Student.StudentId == id);
+            //return context.Grades.First(g => g.StudentId == id);
+        }
+    }
+}
---
+++ WebApiStudent3/WebApiStudent3/Services/StudentServices.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using WebApiStudent3.Models;
+
+namespace WebApiStudent3.Services
+{
+    public class StudentServices
+    {
+        private readonly school3Context context;
+
+        public StudentServices(school3Context context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            /*
+            var first = context.Students.Include(Student => Student.Subjects).ToList();
+            var second = context.Students.Include(Student => Student.Grades).ToList();
+            */
+            return context.Students.Include(Student => Student.Subjects).Include(Student => Student.Grades).ToList();
+        }
+
+        public Student GetStudentByEmail(string email)
+        {
+            return context.Students.First(Student => Student.Email == email);
+        }
+    }
+}
